08488148a19c30d59d10f8b94d01221e80b5a9ff
Will Hastings <will@objectdotcreate.net>
2015-10-18 10:12:44 -0700

    Upgrade React and remove React Router.

--- middleware/renderReact.js
+++ middleware/renderReact.js
@@ -1,34 +1,21 @@
+import { renderToString } from 'react-dom/server';
+import componentRouter from '../lib/componentRouter';
 import React from 'react';
-import Router from 'react-router';
-import routes from '../components/routes.jsx';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
 
 export default function renderReactMiddleware() {
   return function renderReact(req, res, next) {
-    var reactContent = res.reactContent,
-        router,
+    var reactProps = res.reactProps,
+        component = reactProps ? componentRouter(req.path) : null,
         html;
 
-    if (!reactContent) {
+    if (!component) {
       return next();
     }
 
-    // TODO: Memoize router?
-    router = Router.create({routes, location: req.url});
+    html = renderToString(React.createElement(component, reactProps));
 
-    router.run(function(Handler) {
-      var state = {},
-          data;
-      state.content = reactContent;
-
-      html = React.renderToString(
-        React.createElement(Handler, state)
-      );
-
-      data = {html, isProd: IS_PROD};
-
-      res.render('base', data);
-    });
+    res.render('base', {html, isProd: IS_PROD});
   };
 }
--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,9 @@ import loadHtml from '../lib/loadHtml';
 
 export default function homeRoute(app) {
   app.get('/', co.wrap(function*(req, res, next) {
-    res.reactContent = yield loadHtml('pages/home.html');
+    res.reactProps = {
+      content: yield loadHtml('pages/home.html')
+    };
 
     next();
   }));
