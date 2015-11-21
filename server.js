205e33945c82586ce3b6ccaadd0b7c871fa5070a
Will Hastings <will@objectdotcreate.net>
2015-11-21 10:44:21 -0800

    Rename views directory to templates.

--- server.js
+++ server.js
@@ -3,20 +3,28 @@ import builder from 'lib/builder';
 import express from 'express';
 import handlebars from 'express-handlebars';
 import liveReload from 'lib/liveReload';
+import path from 'path';
 import renderReact from 'middleware/renderReact';
 
-const IS_PROD = process.env.NODE_ENV === 'production';
+const IS_PROD = process.env.NODE_ENV === 'production',
+      PORT = 8000,
+      TEMPLATES_DIR = path.join(__dirname, 'templates');
 
 // Load routes.
 import homeRoute from './routes/home';
 import projectsRoute from './routes/projects';
 
-var PORT = 8000;
 
-var app = express();
+const app = express();
 
 // App settings:
-app.engine('.hbs', handlebars({defaultLayout: 'main', extname: '.hbs'}));
+app.set('views', TEMPLATES_DIR);
+app.engine('.hbs', handlebars({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(TEMPLATES_DIR, 'layouts'),
+  partialsDir: path.join(TEMPLATES_DIR, 'partials')
+}));
 app.set('view engine', '.hbs');
 
 // Apply routes.
