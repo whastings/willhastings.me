742670ffc123c713b93e0c67c6e327928f0bb8a0
Will Hastings <will@objectdotcreate.net>
2016-07-31 20:37:08 -0700

    Fix live reload.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const PORT = 8000;
 let serverManager = new ServerManager({staticDir: DIST_DIR});
 serverManager.startServer(PORT);
 
+if (process.env.NODE_ENV === 'development') {
+  require('./lib/liveReload');
+}
+
 process.on('SIGTERM', () => serverManager.stopServer(() => process.exit()));
 // For nodemon:
 process.on('SIGUSR2', () => serverManager.stopServer(() => process.kill(process.pid, 'SIGUSR2')));
