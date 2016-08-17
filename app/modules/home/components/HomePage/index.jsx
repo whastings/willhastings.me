15902326260901c367716d32d8a6e78f13d5146f
Will Hastings <will@objectdotcreate.net>
2016-08-16 20:37:48 -0700

    Add social links to home page.

--- app/modules/home/components/HomePage/index.jsx
+++ app/modules/home/components/HomePage/index.jsx
@@ -1,13 +1,17 @@
 import './styles.scss';
 import React from 'react';
-import { PostList } from 'app/modules/posts/components';
+import SocialLinks from 'home/components/SocialLinks';
+import { PostList } from 'posts/components';
 
 export default function homePage({content, posts}) {
   content = {__html: content};
   return (
     <div className="home-page" id="home-page-content">
       <section className="home-about-me">
-        <div className="wrapper" dangerouslySetInnerHTML={content}></div>
+        <div className="wrapper">
+          <div className="content" dangerouslySetInnerHTML={content}></div>
+          <SocialLinks/>
+        </div>
       </section>
       <section className="home-recent-posts">
         <h2>Recent Posts</h2>
