33c4ac71e10d6678ba79d2defc21b42907866e2e
Will Hastings <will@objectdotcreate.net>
2016-08-26 20:22:28 -0700

    Style projects page for mobile.

--- app/modules/projects/components/ProjectsPage.jsx
+++ app/modules/projects/components/ProjectsPage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function ProjectsPage(props) {
   return (
     <section className="projects-page">
-      <h1>Projects</h1>
+      <h2 className="page-title">Projects</h2>
 
       <ProjectTabs categories={props.categories}/>
     </section>
