e8837b3b7840ad69ef507bde897731be3774a0ea
Will Hastings <will@objectdotcreate.net>
2019-04-20 15:38:47 -0700

    Try out CSS modules

--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styles from './header.module.css';
+
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    className={styles.header}
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+      className={styles.contentContainer}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+          className={styles.link}
         >
           {siteTitle}
         </Link>
