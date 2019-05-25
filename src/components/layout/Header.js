40ba84e99c50a0071382090508c74e5f104b50f0
Will Hastings <will@objectdotcreate.net>
2019-05-25 09:26:51 -0700

    Add social icon links to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import styles from './Header.module.css';
+import GitHubIcon from '../../images/github-icon.svg';
+import LinkedInIcon from '../../images/linkedin-icon.svg';
 
 const Header = ({ siteTitle }) => (
   <header
@@ -13,12 +15,30 @@ const Header = ({ siteTitle }) => (
     >
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
+          to='/'
           className={styles.link}
         >
           {siteTitle}
         </Link>
       </h1>
+      <a
+        className={styles.socialLink}
+        href='https://github.com/whastings'
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label="Will's GitHub"
+      >
+        <GitHubIcon className={styles.socialIcon} />
+      </a>
+      <a
+        className={styles.socialLink}
+        href='https://linkedin.com/in/willhastings4'
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label="Will's LinkedIn"
+      >
+        <LinkedInIcon className={styles.socialIcon} />
+      </a>
     </div>
   </header>
 )
