20bbe7ab0aa9f8a3bced9be6d20f933bab252533
Will Hastings <will@objectdotcreate.net>
2020-03-29 10:35:23 -0700

    Move layout related files around

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout/Layout"
+import SEO from "../components/layout/SEO"
 import FeedItem from '../components/home/FeedItem'
 import styles from './index.module.css'
 
